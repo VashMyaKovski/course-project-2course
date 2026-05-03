dccdf626e91500dee4050d511aa4f1134caeabe2
VMyalo <vsevolod.myalo@gmail.com>
2026-05-03 16:10:13 +0300

    Дописал конфиг, дополнил .env.dev, создал скрипт для разворачивания ollama с моделью внутри.

---
+++ config/__init__.py
@@ -0,0 +1,3 @@
+from config.settings import AppSettings, LlamaFactExtractorSettings, get_settings
+
+__all__ = ["AppSettings", "LlamaFactExtractorSettings", "get_settings"]
---
+++ src/fact_extractor/__init__.py
@@ -0,0 +1,7 @@
+from src.fact_extractor.base import FactExtractorBase
+from src.fact_extractor.llama_instruct_chat import Llama31InstructChatCompletionFactExtractor
+
+__all__ = [
+    "FactExtractorBase",
+    "Llama31InstructChatCompletionFactExtractor",
+]
